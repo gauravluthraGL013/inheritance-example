64100641306c5b50764183f1f948f677afb40d53
gauravluthraGL013 <gauravluthra061@gmail.com>
2021-05-21 13:58:56 +1200

    Add project files.

---
+++ inheritance example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace inheritance_example
+{
+    class Program
+    {
+        //Base Class
+
+        public class vehicle
+        {
+            public vehicle()
+            {
+                Console.WriteLine("i am in the cosntructor vehicle");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
